8911d4ea8064e938551bf6d0642b9d3c724b670b
aniso <aniso@patlaptop.mshome.net>
2023-06-05 12:47:40 +0200

    bugfix service manager

--- milionario/milionario-manager/src/main/java/it/academy/gaming/milionario/test/Test.java
+++ milionario/milionario-manager/src/main/java/it/academy/gaming/milionario/test/Test.java
@@ -2,11 +2,22 @@ package it.academy.gaming.milionario.test;
 
 import java.io.FileNotFoundException;
 
+import it.academy.gaming.milionario.core.domain.Accuratezza;
+import it.academy.gaming.milionario.core.domain.Categoria;
+import it.academy.gaming.milionario.core.domain.Difficolta;
+import it.academy.gaming.milionario.core.domain.Domanda;
+import it.academy.gaming.milionario.core.domain.InformazioniDomanda;
+import it.academy.gaming.milionario.core.domain.Quesito;
+import it.academy.gaming.milionario.core.domain.Quesito.QuesitoBuilder;
+import it.academy.gaming.milionario.core.domain.Risposta;
+import it.academy.gaming.milionario.core.domain.Suggerimento;
 import it.academy.gaming.milionario.core.domain.exceptions.CodiceInvalidoException;
 import it.academy.gaming.milionario.core.domain.exceptions.CreazioneDomandaException;
 import it.academy.gaming.milionario.core.domain.exceptions.CreazioneQuesitoException;
 import it.academy.gaming.milionario.core.domain.exceptions.DifficoltaNonInRangeException;
 import it.academy.gaming.milionario.core.domain.exceptions.NumeroMassimoRisposteSuperatoException;
+import it.academy.gaming.milionario.core.domain.exceptions.SuggerimentiInvalidiException;
+import it.academy.gaming.milionario.core.domain.exceptions.SuggerimentoInvalidoException;
 import it.academy.gaming.milionario.core.domain.exceptions.TestoRispostaAssenteException;
 import it.academy.gaming.milionario.manager.core.application.CvemService;
 import it.academy.gaming.milionario.manager.core.domain.OpzioniPersonaRepository;
@@ -18,7 +29,7 @@ import it.academy.gaming.milionario.manager.grafics.controller.CvemController;
 public class Test {
 	public static void main(String[] args)
 			throws CreazioneDomandaException, TestoRispostaAssenteException, DifficoltaNonInRangeException,
-			NumeroMassimoRisposteSuperatoException, CreazioneQuesitoException, CodiceInvalidoException, FileNotFoundException {
+			NumeroMassimoRisposteSuperatoException, CreazioneQuesitoException, CodiceInvalidoException, FileNotFoundException, SuggerimentiInvalidiException, SuggerimentoInvalidoException {
 		String url = "jdbc:mysql://localhost:3306/milionario";
 		String user = "root";
 		String password = "admin";
@@ -29,39 +40,62 @@ public class Test {
 		CvemService service = new CvemService(repository,opzioniPersonaRepository);
 		CvemController controller = new CvemController(service);
 		controller.showMenuScreen();
+		
+		
+		
 
 //		QuesitoBuilder builder = Quesito.builder();
 //
 //		InformazioniDomanda informazioniDomanda = new InformazioniDomanda(
-//				"https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRBKouHO57uTjWH-ImFXgHxryC617ISal9NYFkomALCV2o0EgG5DqT1myoE1D5Ehi7a",
+//				null,
 //				null);
 //
-//		Domanda domanda = new Domanda("Quanti pianeti ci sono nel sistema solare?", Categoria.SCIENZA,
+//		Domanda domanda = new Domanda("Chi e' il Padre di Zeus?", Categoria.STORIA,
 //				informazioniDomanda);
 //		builder.setDomanda(domanda);
 //
-//		builder.setDifficolta(new Difficolta(4));
+//		builder.setDifficolta(new Difficolta(8));
+//		
+//		/*
+//		 * aggiu ngo le risposte
+//		 */
 //
-//		Risposta risposta = Risposta.crea("9", false);
+//		Risposta risposta = Risposta.crea("Ares", false);
 //		builder.aggiungiRisposta(risposta);
 //
-//		risposta = Risposta.crea("7", false);
+//		risposta = Risposta.crea("Urano", false);
 //		builder.aggiungiRisposta(risposta);
 //
-//		risposta = Risposta.crea("10", false);
+//		risposta = Risposta.crea("Ade", false);
 //		builder.aggiungiRisposta(risposta);
 //
-//		risposta = Risposta.crea("8", true);
+//		risposta = Risposta.crea("Crono", true);
 //		builder.aggiungiRisposta(risposta);
-//
+//		
+//		
+//		/*
+//		 * aggiungo i suggerimenti
+//		 */
+//		Suggerimento suggerimento=Suggerimento.crea(" ${N} la risposta giusta sono sicura sia la ${Y} ", 15, Accuratezza.CORRETTA);
+//		builder.aggiungiSuggerimento(suggerimento);
+//		
+//		suggerimento=Suggerimento.crea(" Sono indeciso tra la ${Y} e la ${X}", 10, Accuratezza.IMPRECISA);
+//		builder.aggiungiSuggerimento(suggerimento);
+//		
+//		suggerimento=Suggerimento.crea(" ${N} la risposta giusta sono sicura sia la ${X} ", 13, Accuratezza.SBAGLIATA);
+//		builder.aggiungiSuggerimento(suggerimento);
+//		
+//		suggerimento=Suggerimento.crea(" \"${N} mi dispiace non so proprio quale sia la risposta giusta ", 15, Accuratezza.ASTENUTA);
+//		builder.aggiungiSuggerimento(suggerimento);
+//		
 //		Quesito quesito = builder.build();
 //		repository.add(quesito);
-
+//
 //		Collection<Quesito> quesiti = repository.findByLivelloDifficolta(new Difficolta(10));
 //		System.out.println();
-
+//
 //		List<Risposta> nuoveRisposte = new ArrayList<>();
-
+//
 //		informazioniDomanda = new InformazioniDomanda(
 //				"https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRBKouHO57uTjWH-ImFXgHxryC617ISal9NYFkomALCV2o0EgG5DqT1myoE1D5Ehi7a",
 //				"https://it.wikipedia.org/wiki/Sistema_solare");
@@ -69,7 +103,7 @@ public class Test {
 //		domanda = new Domanda("Ciao?", Categoria.STORIA, informazioniDomanda);
 //
 //		repository.setDomanda(CodiceQuesito.parse("bWnUG3tl"), domanda);
-
+//
 //		repository.remove(CodiceQuesito.parse("bpwhApo9"));
 
 	}
